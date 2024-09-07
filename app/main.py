8bd008dedfeaa6f173ea373b34fb2e582b3080ea
MoonCPU <moonkang20@gmail.com>
2024-09-07 13:13:34 -0300

    changing http method from post to get

--- app/main.py
+++ app/main.py
@@ -1,20 +1,35 @@
 from fastapi import FastAPI, HTTPException
 from dotenv import load_dotenv
 import os
-load_dotenv()
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware  # Import CORSMiddleware
 from app.indicators import scrape_indicators
 from app.netGrowth import scrape_netGrowth
+from mangum import Mangum
+
+load_dotenv()
 
 app = FastAPI()
+handler = Mangum(app)
 
-@app.post("/scrape/indicators/{stock_code}")
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Temporarily allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/scrape/indicators/{stock_code}")
 async def scrape_indicators_endpoint(stock_code: str):
     if not stock_code:
         raise HTTPException(status_code=400, detail="stock_code is required")
 
     try:
-        indicators_data = scrape_indicators(stock_code)
+        # Call the asynchronous scraping function
+        indicators_data = await scrape_indicators(stock_code)
 
         response_data = {
             "indicators": indicators_data,
@@ -25,13 +40,14 @@ async def scrape_indicators_endpoint(stock_code: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
-@app.post("/scrape/netGrowth/{stock_code}")
+@app.get("/scrape/netGrowth/{stock_code}")
 async def scrape_netGrowth_endpoint(stock_code: str):
     if not stock_code:
         raise HTTPException(status_code=400, detail="stock_code is required")
 
     try:
-        net_growth_data = scrape_netGrowth(stock_code)
+        # Call the asynchronous scraping function
+        net_growth_data = await scrape_netGrowth(stock_code)
         
         response_data = {
             "net_growth": net_growth_data
